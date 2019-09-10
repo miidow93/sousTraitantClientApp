b82b008e5da504f65d2858e8da9a4a39fb13efee
BiyiAbdeladem <biyi.abdeladem95@gmail.com>
2019-09-10 17:26:13 +0100

    posteprincipale soustraitant

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# OurVisitorApp
+# sousTraitantApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ourVisitorApp'`, () => {
+  it(`should have as title 'sousTraitantApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ourVisitorApp');
+    expect(app.title).toEqual('sousTraitantApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ourVisitorApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sousTraitantApp!');
   });
 });
